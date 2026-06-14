5d7fa8e3ffe89de3dc8980beb86a77c2afc0149e
Levhcwm21. <alexags1983@gmail.com>
2026-06-14 15:57:44 +0530

    Change default UPSTREAM_BRANCH from 'wzv3' to 'lxhem03'

--- update.py
+++ update.py
@@ -58,14 +58,14 @@ if DATABASE_URL := config_file.get("DATABASE_URL", "").strip():
             old_config is not None and old_config == config_file or old_config is None
         ) and config_dict is not None:
             config_file["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
-            config_file["UPSTREAM_BRANCH"] = config_dict.get("UPSTREAM_BRANCH", "wzv3")
+            config_file["UPSTREAM_BRANCH"] = config_dict.get("UPSTREAM_BRANCH", "lxhem03")
             config_file["UPDATE_PKGS"] = config_dict.get("UPDATE_PKGS", "True")
         conn.close()
     except Exception as e:
         log_error(f"Database ERROR: {e}")
 
 UPSTREAM_REPO = config_file.get("UPSTREAM_REPO", "").strip()
-UPSTREAM_BRANCH = config_file.get("UPSTREAM_BRANCH", "").strip() or "wzv3"
+UPSTREAM_BRANCH = config_file.get("UPSTREAM_BRANCH", "").strip() or "lxhem03"
 
 if UPSTREAM_REPO:
     if path.exists(".git"):
